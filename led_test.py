97e5a65fda4bf12f8d2d37e7757e8948fda7e3e0
JM <joerg_mueller26@hotmail.com>
2022-06-17 21:54:53 +0200

    led_test added

---
+++ led_test.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import sys
+
+
+pinLed = 5
+ledOn = Fasle
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pinLed, GPIO.OUT)
+
+
+def change_led(value):
+    if value == 'y':
+        GPIO.output(pinLed, True)
+        print("Led is on")
+    elif value == 'n':
+        GPIO.output(pinLed, False)
+        print("Led is off")
+    else:
+        GPIO.output(pinLed, False)
+        print("Led is off")
+
+
+while True:
+    try:
+        inputValue = input("y oder n für Led wechsel")
+        change_led(inputValue)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        sys.exit()
+
