35669ddf386f4f9599587a90194a49f4fd356a67
JM <joerg_mueller26@hotmail.com>
2022-06-26 22:14:57 +0200

    rename structure

--- mainapp/main.py
+++ mainapp/main.py
@@ -5,8 +5,6 @@ import RPi.GPIO as GPIO
 import controller.motorcontroller as motor
 
 
-GPIO.setmode(GPIO.BCM)
-
 EnableLeft = 13
 EnableRight = 15
 InputLeft1 = 29
@@ -17,12 +15,13 @@ InputRight4 = 35
 leftMotor = 0
 rightMotor = 1
 
-GPIO.setmode(EnableLeft, GPIO.OUT)
-GPIO.setmode(EnableRight, GPIO.OUT)
-GPIO.setmode(InputLeft1, GPIO.OUT)
-GPIO.setmode(InputLeft2, GPIO.OUT)
-GPIO.setmode(InputRight3, GPIO.OUT)
-GPIO.setmode(InputRight4, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(EnableLeft, GPIO.OUT)
+GPIO.setup(EnableRight, GPIO.OUT)
+GPIO.setup(InputLeft1, GPIO.OUT)
+GPIO.setup(InputLeft2, GPIO.OUT)
+GPIO.setup(InputRight3, GPIO.OUT)
+GPIO.setup(InputRight4, GPIO.OUT)
 
 motor = motor.MotorController(InputLeft1, InputLeft2, InputRight3, InputRight4)
 
@@ -37,8 +36,3 @@ def mainloop():
 
 if __name__ == "__main__":
     mainloop()
-
-
-
-
-
