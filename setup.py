7dd5036cd1110c8a84672e93241b0bbb84acff69
JM <joerg_mueller26@hotmail.com>
2022-06-30 21:15:07 +0200

    rename structure

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='robopy',
+    version='0.1.1',
+    description='simple lib for robo stuff',
+    author='joemaydo',
+    url='https://github.com/joemaydo/robopy.git',
+    packages=['robopy'],
+
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Operating System :: POSIX :: Windows'
+        'Programming Language :: Python :: 3.9'
+    ],
+)
