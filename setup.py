ca67976b816e94b97604402726802740976ea5c3
JM <joerg_mueller26@hotmail.com>
2022-07-02 21:32:42 +0200

    new install repo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='robopy',
-    version='0.1.4',
+    version='0.1.5',
     description='simple lib for robo stuff',
     author='joemaydo',
     author_email='joerg_mueller26@hotmail.com',
